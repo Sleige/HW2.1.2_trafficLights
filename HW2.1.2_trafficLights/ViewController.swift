73993c1e7dbd607f29053a45f5d55bd12c5f7f3f
Vsevolod Kondratev <vsevolod.kondratev@gmail.com>
2021-10-21 00:12:13 +0700

    Initial Commit

---
+++ HW2.1.2_trafficLights/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW2.1.2_trafficLights
+//
+//  Created by Всеволод Кондратьев on 21.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
