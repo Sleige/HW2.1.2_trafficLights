1df95ccda134c9a1186c9bec3c9e78bec2fe66e3
Vsevolod Kondratev <vsevolod.kondratev@gmail.com>
2021-10-21 03:03:03 +0700

    finally done

--- HW2.1.2_trafficLights/ViewController.swift
+++ HW2.1.2_trafficLights/ViewController.swift
@@ -30,6 +30,22 @@ class ViewController: UIViewController {
 
     @IBAction func switchLight() {
         buttonStart.setTitle("NEXT", for: .normal)
+        
+        if red.alpha == 1 {
+            red.alpha = 0.3
+            yellow.alpha = 1
+            return
+        } else if yellow.alpha == 1 {
+            yellow.alpha = 0.3
+            green.alpha = 1
+            return
+        } else if green.alpha == 1 {
+            green.alpha = 0.3
+            red.alpha = 1
+            return
+        } else {
+            red.alpha = 1
+        }
     }
     
 }
